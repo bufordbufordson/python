fe1fc476d5723b2adf10b497bc2d08581da4d813
Aren W <71801283+bufordbufordson@users.noreply.github.com>
2024-06-17 15:35:32 -0700

    Update myprogram.py

--- myprogram.py
+++ myprogram.py
@@ -1,41 +1,46 @@
-primitivesp_nos = set(); primes = [] # Set of primitive semiperfect (non-weird) numbers (any multiple is semiperfect) and primes 
+from time import time
+start = time()
+
+primitivesp_nos = set(); primes = []
 
 def main(): 
-    x = 2; weird_nos = []
-    n = 1766  # First n weird numbers to be found 
+    x = 2; weird_nos = []; n = 1766
     while n > 0:
-        if isweird(x) == 1: weird_nos.append(x); n = n - 1 # n is weird 
+        if isweird(x) == 1: weird_nos.append(x); n = n - 1
         x = x + 1        
     print("First", len(weird_nos), "weird nos:\n", weird_nos)
 
 def isweird(n): 
     global primes; global primitivesp_nos; pr_fctrs = []; a = n 
-    for i in primes: 
-        while a % i == 0: pr_fctrs.append(i); a = a//i # Divides number by each prime factor found 
-        if i * i > a: break # Factors above square root repeat 
+    for i in primes:
+        while a % i == 0: pr_fctrs.append(i); a = a//i
+        if i * i > a: break
     if a > 1: pr_fctrs.append(a)
-    if a == n: primes.append(n); return 0  # Saves primes to reduce the number of tests for future numbers 
-    sum_fctrs = 1; divisors = set(pr_fctrs) 
-    for i in divisors: sum_fctrs = sum_fctrs*(i**(pr_fctrs.count(i)+1)-1)//(i-1) # Sum of factors formula 
-    difference = sum_fctrs - 2 * n
-    if difference <= 0: # Difference < 0 is deficient; difference = 0 is semiperfect 
-        if difference == 0: primitivesp_nos.add(n)
+    if a == n: primes.append(n); return 0 
+    sum_fctrs = 1; divs = set(pr_fctrs) 
+    for i in divs: sum_fctrs = sum_fctrs*(i**(pr_fctrs.count(i)+1)-1)//(i-1)
+    df = sum_fctrs - 2 * n
+    if df <= 0: 
+        if df == 0: primitivesp_nos.add(n)
         return 0
     # Next 10 lines from Jerome Richard: stackoverflow.com/questions/6800193
-    divisors = [1]; last_prime = 0; fctr = 0; slice_len = 0
+    divs = [1]; last_prime = 0; fctr = 0; slice_len = 0
     for prime in pr_fctrs:
-        if last_prime != prime: slice_len = len(divisors); fctr = prime
+        if last_prime != prime: slice_len = len(divs); fctr = prime
         else: fctr *= prime
         for i in range(slice_len):
-            d = divisors[i] * fctr
-            if d not in primitivesp_nos: divisors.append(d) # n has semiperfect factor = n is semiperfect 
+            d = divs[i] * fctr
+            if d not in primitivesp_nos: divs.append(d)
             else: return 0 
         last_prime = prime
-    x = 1; divisors = set(i for i in divisors if i <= difference) # Removes factors that must be in combination summing to n 
-    ns = n - (difference + n - sum(divisors)) # without them, the max. remaining sum is too low 
-    if ns < 0: return 1 # Positive factors cannot sum to a negative number  
-    for d in divisors: x |= x << d # Bitset 
+    x = 1; divs.remove(divs[-1]); divs = set(i for i in divs if i <= df)
+    ns = n - (df + n - sum(divs))
+    if ns < 0: return 1
+    for d in divs: x |= x << d
     if x >> ns & 1: primitivesp_nos.add(n); return 0
-    else: return 1 # n is weird 
+    else: return 1 
     
 main()
+
+end = time() 
+print("Execution time: ", round(end - start, 2), "s")
