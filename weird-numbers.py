a62ba524bd8992f74b6109bf82bfb91db31e06e1
barrybecker4 <barrybecker4@gmail.com>
2024-07-01 02:48:10 -0700

    Avoid the use of ;
    - ; should not be needed in python programs. The code is more readable without it. Put statements on separate lines
    - You are not using devcontainer, so I removed it.

--- weird-numbers.py
+++ weird-numbers.py
@@ -1,4 +1,4 @@
-semi_perfects = set();
+semi_perfects = set()
 primes = []
 
 
@@ -27,30 +27,35 @@ def print_weird_numbers(n):
 
 
 def isweird(x):
-    global primes;
-    global semi_perfects;
-    prime_divisors = [];
+    global primes
+    global semi_perfects
+    prime_divisors = []
     a = x
     for i in primes:
-        while a % i == 0: prime_divisors.append(i); a = a // i
+        while a % i == 0:
+            prime_divisors.append(i)
+            a = a // i
         if i * i > a: break
     if a > 1:
         prime_divisors.append(a)
-        if a == x: primes.append(x); return 0
-    sum_fctrs = 1;
+        if a == x:
+            primes.append(x)
+            return 0
+    sum_fctrs = 1
     divs = set(prime_divisors)
-    for i in divs: sum_fctrs = sum_fctrs * (i ** (prime_divisors.count(i) + 1) - 1) // (i - 1)
+    for i in divs:
+        sum_fctrs = sum_fctrs * (i ** (prime_divisors.count(i) + 1) - 1) // (i - 1)
     df = sum_fctrs - 2 * x
     if df <= 0:
         if df == 0: semi_perfects.add(x)
         return 0
-    divs = [1];
-    last_prime = 0;
-    fctr = 0;
+    divs = [1]
+    last_prime = 0
+    fctr = 0
     slice_len = 0
     for prime in prime_divisors:
         if last_prime != prime:
-            slice_len = len(divs);
+            slice_len = len(divs)
             fctr = prime
         else:
             fctr *= prime
@@ -61,14 +66,14 @@ def isweird(x):
             else:
                 return 0
         last_prime = prime
-    x = 1;
-    divs.pop(-1);
+    x = 1
+    divs.pop(-1)
     divs = {i for i in divs if i <= df}
     target = x - (df + x - sum(divs))
     if target < 0: return 1
     for d in divs: x |= x << d
     if x >> target & 1:
-        semi_perfects.add(x);
+        semi_perfects.add(x)
         isweird = 0
     else:
         isweird = 1
